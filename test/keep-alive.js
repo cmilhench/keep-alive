cbe36c10679afafccd1990771220c63c033163ff
Richard van der Dys <richard.vanderdys@gmail.com>
2016-05-22 13:50:04 -0500

    - Removed unneeded request dependency (makes the package lighter) - Added simple http get request (was commented out) - Added get request test

--- test/keep-alive.js
+++ test/keep-alive.js
@@ -1,8 +1,32 @@
 var keepAlive = require('..');
 var assert = require('assert');
+var nock = require('nock');
 
 describe('Keep Alive', function(){
-  
+
+  before(function() {
+    nock.disableNetConnect();
+  });
+
+  after(function() {
+    nock.enableNetConnect();
+  });
+
+  describe('request', function(){
+    this.timeout(3000);
+    it('should make http get request', function(done){
+      var request = nock('http://www.test.com')
+        .get('/ping')
+        .once()
+        .reply(200, 'Test Response');
+      keepAlive('http://www.test.com/ping', 2000);
+      setTimeout(function () {
+        assert(request.isDone());
+        done();
+      }, 2000);
+    });
+  });
+
   describe('ctor', function(){
     it('should call add', function(){
       var called = 0;
@@ -13,7 +37,7 @@ describe('Keep Alive', function(){
       assert(called === 1);
     });
   });
-  
+
   describe('add', function(){
     it('should add keepAlive', function(){
       var keepAlives = keepAlive.add('key', 6000).list();
@@ -22,7 +46,7 @@ describe('Keep Alive', function(){
     it('in v2, should recalculate frequencies');
     it('in v2, should recalculate iterations');
   });
-  
+
   describe('remove', function(){
     it('should remove keepAlive', function(){
       var keepAlives = keepAlive.add('key', 6000).remove('key').list();
@@ -31,12 +55,12 @@ describe('Keep Alive', function(){
     it('in v2, should recalculate frequencies');
     it('in v2, should recalculate iterations');
   });
-  
+
   describe('list', function(){
     it('should return keepAlives', function(){
       var keepAlives = keepAlive.list();
       assert(keepAlives !== undefined);
     });
   });
-  
+
 });
